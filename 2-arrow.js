005936d33874037138de09d703e8997a7bf93594
Angel Velazquez <angelvelazquez.op@gmail.com>
2024-11-12 13:04:21 -0400

    change add function

--- 2-arrow.js
+++ 2-arrow.js
@@ -1,8 +1,3 @@
-  /**
-   * This function returns an object with a list of San Francisco neighborhoods.
-   * It also has an addNeighborhood method that adds a new neighborhood to the list.
-   * @returns {object} An object with a list of San Francisco neighborhoods and an addNeighborhood method.
-   */
 export default function getNeighborhoodsList() {
     this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
   
