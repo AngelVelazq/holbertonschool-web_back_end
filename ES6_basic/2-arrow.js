12942da408f565c2dc859764953b1d0944473e25
Angel Velazquez <angelvelazquez.op@gmail.com>
2024-11-12 15:55:57 -0400

    adding comments

--- ES6_basic/2-arrow.js
+++ ES6_basic/2-arrow.js
@@ -1,13 +1,9 @@
 export default function getNeighborhoodsList() {
-    const sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
     this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
   
     this.addNeighborhood = (newNeighborhood) => {
-      // Add the new neighborhood to the list
-      sanFranciscoNeighborhoods.push(newNeighborhood);
-      // Return the updated list
-      return sanFranciscoNeighborhoods;
       this.sanFranciscoNeighborhoods.push(newNeighborhood);
+      /* eslint-disable no-console */
       return this.sanFranciscoNeighborhoods;
     };
   }
