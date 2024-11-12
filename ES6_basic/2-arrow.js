79156d6243531a50a376f3c8161de2d2d77f1759
Angel Velazquez <angelvelazquez.op@gmail.com>
2024-11-12 15:53:02 -0400

    adding coments

--- ES6_basic/2-arrow.js
+++ ES6_basic/2-arrow.js
@@ -1,3 +1,9 @@
+/**
+ * Returns an object with a list of San Francisco neighborhoods.
+ * The returned object has a single method, addNeighborhood,
+ * which takes a new neighborhood name and adds it to the list.
+ * @return {Object} - An object with addNeighborhood method, and a list of neighborhoods.
+ */
 export default function getNeighborhoodsList() {
     this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
   
