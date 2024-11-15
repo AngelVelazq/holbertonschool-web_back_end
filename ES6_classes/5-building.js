045eada47f8f3775ef1b771deff7b05074ed1b30
Angel Velazquez <angelvelazquez.op@gmail.com>
2024-11-15 12:29:19 -0400

    final commit

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,42 +1,15 @@
-class Building {
-    def __init__(self, sqft) {
-        self._sqft = sqft
+export default class Building {
+  /**
+     * @param {Number} sqft
+     */
+  constructor(sqft) {
+    this._sqft = sqft;
+    if (this.constructor !== Building && this.evacuationWarningMessage === undefined) {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
     }
+  }
 
-    @property
-    def sqft(self) {
-        return self._sqft
-    }
-
-    def evacuationWarningMessage(self) {
-        raise NotImplementedError("Class extending Building must override evacuationWarningMessage")
-    }
-}
-
-class OfficeBuilding(Building) {
-    def __init__(self, sqft) {
-        super().__init__(sqft)
-    }
-
-    def evacuationWarningMessage(self) {
-        return "Please evacuate the office building immediately."
-    }
-}
-
-class ResidentialBuilding(Building) {
-    def __init__(self, sqft) {
-        super().__init__(sqft)
-    }
-}
-
-try {
-    office = OfficeBuilding(1000)
-    console.log(office.sqft)
-    console.log(office.evacuationWarningMessage())
-
-    residential = ResidentialBuilding(500)
-    console.log(residential.sqft)
-    console.log(residential.evacuationWarningMessage())
-} except (e) {
-    console.log(e)
+  get sqft() {
+    return this._sqft;
+  }
 }
