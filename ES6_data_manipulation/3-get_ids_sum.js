ed34239e9c1654a3f578d56238cf0041e2784868
Angel Velazquez <angelvelazquez.op@gmail.com>
2024-11-21 14:54:18 -0400

    sum of all students id

---
+++ ES6_data_manipulation/3-get_ids_sum.js
@@ -0,0 +1,7 @@
+import getListStudents from './0-get_list_students.js';
+
+export default function getStudentIdsSum(students) {
+    return students.reduce((sum, student) => sum + student.id, 0);
+}
+
+console.log(getStudentIdsSum(getListStudents()));
