9bd6ba58079670b0e2040f296c562965dacd6972
Angel Velazquez <angelvelazquez.op@gmail.com>
2024-11-21 15:08:27 -0400

    fix in indentation

--- ES6_data_manipulation/3-get_ids_sum.js
+++ ES6_data_manipulation/3-get_ids_sum.js
@@ -1,7 +1,5 @@
-import getListStudents from './0-get_list_students.js';
+import getListStudents from './0-get_list_students';
 
 export default function getStudentIdsSum(students) {
     return students.reduce((sum, student) => sum + student.id, 0);
 }
-
-console.log(getStudentIdsSum(getListStudents()));
