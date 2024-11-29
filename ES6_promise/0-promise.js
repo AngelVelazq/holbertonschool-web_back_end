a5c3d0ac620e1ba46ab61806fc9663f3b7d4b9f1
Angel Velazquez <angelvelazquez.op@gmail.com>
2024-11-28 20:39:20 -0400

    fix in code

--- ES6_promise/0-promise.js
+++ ES6_promise/0-promise.js
@@ -1,3 +1,7 @@
-export default function getResponse() {
-    return getResponseFromAPI();
+export default function getResponseFromAPI() {
+    return new Promise((resolve, reject) => {
+      // Optionally simulate async behavior or always resolve for this task
+      resolve('Success');
+    });
   }
+  
