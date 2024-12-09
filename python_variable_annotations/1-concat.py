1826514de117b9496da90bf14fbd94c97a81901a
AngelVelazq <angelvelazquez.op@gmail.com>
2024-12-09 13:04:46 -0400

    concat string

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+"""
+This is the documentation for the module 1-concat.
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Returns the concatenation of two strings.
+
+    Args:
+        str1 (str): The first string.
+        str2 (str): The second string.
+
+    Returns:
+        str: The concatenation of str1 and str2.
+    """
+    return str1 + str2
