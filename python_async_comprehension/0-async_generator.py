07aae325a54607609a98d9743d8be69553a78770
AngelVelazq <angelvelazquez.op@gmail.com>
2024-12-09 14:26:13 -0400

    init

---
+++ python_async_comprehension/0-async_generator.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+import asyncio
+import random
+
+
+async def async_generator():
+    """
+    Asynchronous generator that yields random numbers between 0 and 10.
+
+    Yields:
+        int: A random integer between 0 and 10.
+    """
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.randint(0, 10)
+
+
+async def main():
+    """
+    Example usage of async_generator.
+    """
+    async for num in async_generator():
+        print(num)
+
+if __name__ == "__main__":
+    asyncio.run(main())
