6f37ff83760901ce1b5618126744d69bf6c637cb
AngelVelazq <angelvelazquez.op@gmail.com>
2024-12-11 16:39:42 -0400

    >

--- pagination/2-hypermedia_pagination.py
+++ pagination/2-hypermedia_pagination.py
@@ -1,29 +1,25 @@
 #!/usr/bin/env python3
-"""Pagination utilities."""
-
+""" File that contains functions for pagination """
 import csv
 import math
 from typing import List, Tuple, Dict
 
 
-def index_range(page: int, page_size: int) -> tuple:
-    """Calculate the start and end index for pagination."""
-    start = (page - 1) * page_size
-    end = page * page_size
-    return start, end
+def index_range(page: int, page_size: int) -> Tuple:
+    """ Function that returns the index and size of a page """
+    return (((page * page_size) - page_size), (page * page_size))
 
 
 class Server:
-    """Server class to paginate a database of popular baby names.
-    """
+    """ Server class to paginate a database of popular baby names. """
+
     DATA_FILE = "Popular_Baby_Names.csv"
 
     def __init__(self):
         self.__dataset = None
 
     def dataset(self) -> List[List]:
-        """Cached dataset
-        """
+        """ Cached dataset """
         if self.__dataset is None:
             with open(self.DATA_FILE) as f:
                 reader = csv.reader(f)
@@ -33,14 +29,13 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-        """Displays the correct amount of items per page"""
+        """ Displays the correct amount of items per page """
         assert type(page) is int and page > 0
         assert type(page_size) is int and page_size > 0
         data = Server.dataset(self)
         pp = index_range(page, page_size)
         return data[pp[0]:pp[1]]
-    
-    
+
     def get_hyper(self, page: int = 1, page_size: int = 10) -> Dict:
         """ Displays the page information """
         data = Server.dataset(self)
@@ -60,5 +55,4 @@ class Server:
             "prev_page": prev_page,
             "total_pages": total_pages
         }
-
         return page_info
