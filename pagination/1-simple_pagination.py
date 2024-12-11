713a81f74846c9813a33812bde7556ea7bee5ed8
AngelVelazq <angelvelazquez.op@gmail.com>
2024-12-11 15:04:33 -0400

    use csv file & establish parameters

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -20,12 +20,19 @@ def index_range(page: int, page_size: int) -> tuple:
     return start, end
 
 
+def read_csv_file(filename: str) -> list:
+    """Read a CSV file.  Returns: list: The data from the file"""
+    with open(filename, 'r') as file:
+        reader = csv.reader(file)
+        return list(reader)
+
+
 def get_page(data: list, page: int = 1, page_size: int = 10) -> list:
     """Get the items for the current page."""
     assert isinstance(page, int) and page > 0
-    "page must be an integer greater than 0"
+    """page must be an integer greater than 0"""
     assert isinstance(page_size, int) and page_size > 0
-    "page_size must be an integer greater than 0"
+    """page_size must be an integer greater than 0"""
 
     start, end = index_range(page, page_size)
     if start >= len(data) or end > len(data):
