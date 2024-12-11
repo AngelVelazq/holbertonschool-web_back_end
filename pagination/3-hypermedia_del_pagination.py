75995168838aaa2319e63127364a492efeb17b75
AngelVelazq <angelvelazquez.op@gmail.com>
2024-12-11 16:47:22 -0400

    send it

--- pagination/3-hypermedia_del_pagination.py
+++ pagination/3-hypermedia_del_pagination.py
@@ -39,5 +39,31 @@ class Server:
             }
         return self.__indexed_dataset
 
-    def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
-            pass
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> dict:
+        """Returns a dictionary containing pagination metadata."""
+        data = self.get_page(page, page_size)
+        total_pages = math.ceil(len(self.dataset()) / page_size)
+        return {
+        'page_size': page_size,
+        'page': page,
+        'data': data,
+        'next_page': page + 1 if page < total_pages else None,
+        'prev_page': page - 1 if page > 1 else None,
+        'total_pages': total_pages
+    }
+        
+        
+    def get_hyper_index(self, index: int = None, page_size: int = 10) -> dict:
+        """Returns a dictionary containing pagination metadata."""
+        if index is None:
+            index = 0
+        
+        dataset_length = len(self.dataset())
+        data = self.dataset()[index:index+page_size]
+        
+        return {
+            'index': index,
+            'next_index': index + page_size if index + page_size < dataset_length else None,
+            'page_size': page_size,
+            'data': data
+        }
