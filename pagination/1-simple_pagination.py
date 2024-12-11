b48b29f4d4ae6a3f5602617048562d8867f4ed4d
AngelVelazq <angelvelazquez.op@gmail.com>
2024-12-11 15:49:11 -0400

    use csv file & establish parameters

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -2,29 +2,35 @@
 """Pagination utilities."""
 
 import csv
+import math
+from typing import List
 
 
 def index_range(page: int, page_size: int) -> tuple:
-    """
-    Calculate the start and end index for pagination.
-
-    Args:
-        page (int): The current page number.
-        page_size (int): The number of items per page.
-
-    Returns:
-        tuple: A tuple containing the start and end index.
-    """
+    """Calculate the start and end index for pagination."""
     start = (page - 1) * page_size
     end = page * page_size
     return start, end
 
 
-def read_csv_file(filename: str) -> list:
-    """Read a CSV file.  Returns: list: The data from the file"""
-    with open(filename, 'r') as file:
-        reader = csv.reader(file)
-        return list(reader)
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
 
 
 def get_page(data: list, page: int = 1, page_size: int = 10) -> list:
@@ -38,7 +44,3 @@ def get_page(data: list, page: int = 1, page_size: int = 10) -> list:
     if start >= len(data) or end > len(data):
         return []
     return data[start:end]
-
-
-filename = "data.csv"
-data = read_csv_file(filename)
